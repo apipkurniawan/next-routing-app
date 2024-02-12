ac8a1e5468ccd262ab5f3c5b2ad7c42c8162f7c7
Apip Kurniawan <davitaja92@gmail.com>
2024-02-12 10:08:05 +0700

    add metadata

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import EventList from "../components/events/event-list";
 import { getFeaturedEvents } from "../helpers/api-utils";
 
@@ -5,6 +6,10 @@ import { getFeaturedEvents } from "../helpers/api-utils";
 function HomePage(props) {
   return (
     <div>
+      <Head>
+        <title>Next app course</title>
+        <meta name="description" content="find a lot of great events..." />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
