34e2f4d1b17c79dc980b0595f2e8fb5751e7da8e
Apip Kurniawan <davitaja92@gmail.com>
2024-02-15 10:36:20 +0700

    client-side page guard

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,8 +1,25 @@
+import { getSession } from "next-auth/client";
 import EventItem from "./event-item";
 import classes from "./event-list.module.css";
+import { useEffect, useState } from "react";
 
 function EventList(props) {
   const { items } = props;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        window.location.href = "/auth";
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
+  if (isLoading) {
+    return <p className={classes.list}>Loading...</p>;
+  }
 
   return (
     <ul className={classes.list}>
